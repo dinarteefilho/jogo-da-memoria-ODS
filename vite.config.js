4090e47afc895f6633fefe66c5c5e3696760cfc1
Dinarte Filho <dinarte_filho@hotmail.com>
2025-09-27 17:51:16 -0300

    Corrige configuração base para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jogo-da-memoria-ODS/", // <-- ADICIONE ESTA LINHA!
+  base: "/jogo-da-memoria-ODS/",
 })
